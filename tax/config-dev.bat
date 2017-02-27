58ee7a1fa0c5906fc7caeb0730dbdacad74b244d
Erlis Vidal <erlis_vidal@ultimatesoftware.com>
2017-02-27 17:17:09 -0500

    ADDED: bundle install in config-dev script

--- tax/config-dev.bat
+++ tax/config-dev.bat
@@ -8,6 +8,7 @@ rem setting up development environment
 rem ~~~~~~~~~~~~~~~~~~~~~~~~~
 @pushd %~dp0
 cd c:\Projects\tax-management-core\BuildAndDeployment\
+bundle install
 bundle exec rake setup_local_dev_env
 @popd
 
