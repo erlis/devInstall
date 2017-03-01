8f3ac046dfd70f7aa662120f47c82df35b71f55b
Erlis Vidal <erlis_vidal@ultimatesoftware.com>
2017-03-01 16:09:29 -0500

    FIXED: config-dev.bat

--- dev-essentials/erlis-pick.bat
+++ dev-essentials/erlis-pick.bat
@@ -4,3 +4,7 @@ call install-slack.bat
 call install-7zip.bat
 call install-totalcommander.bat
 call "net tools\install-curl.bat"
+
+choco install launchy
+choco install conemu
+
--- tax/config-dev.bat
+++ tax/config-dev.bat
@@ -9,8 +9,12 @@ rem ~~~~~~~~~~~~~~~~~~~~~~~~~
 @pushd %~dp0
 cd c:\Projects\tax-management-core\BuildAndDeployment\
 call bundle install
+@popd
+
 call config-ptm-db.bat
 
+@pushd %~dp0
+cd c:\Projects\tax-management-core\BuildAndDeployment\
 call bundle exec rake dev:setup_apphost
 call bundle exec rake dev:setup_iis
 @popd
