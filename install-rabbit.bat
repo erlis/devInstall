44cdae37733412b37669afb0317592529f606668
Erlis Vidal <erlis_vidal@ultimatesoftware.com>
2017-03-30 11:18:46 -0400

    FIXED: install-rabbit simplified

--- install-rabbit.bat
+++ install-rabbit.bat
@@ -2,5 +2,4 @@ cmd /c cinst python2
 @powershell -NoProfile -ExecutionPolicy unrestricted -Command^
     ".\append_path.ps1 'C:\tools\python2;C:\tools\python2\Scripts'"
 	
-cmd /c cinst erlang -Version 17.4.0
 cmd /c cinst rabbitmq -Version 3.6.5
